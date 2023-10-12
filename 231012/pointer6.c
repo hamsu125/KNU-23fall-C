dfd86c26c38e01ab0e7c627c20b332018f1adc94
hamsu125 <hamsj2413@naver.com>
2023-10-12 11:22:13 +0900

    Update pointer6.c

--- 231012/pointer6.c
+++ 231012/pointer6.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
+void swap(int* p1, int* p2)
+{
+	int test = *p1;
+	*p1 = *p2;
+	*p2 = test;
+}
+
 void sort(int *p)
 {
 	int i, j, temp;
 	for (i = 0; i < 5 - 1; i++) {
 		for (j = i + 1; j < 5; j++) {
 			if (*(p + i) > *(p + j)) {
-				temp = *(p + i);
-				*(p + i) = *(p + j);
-				*(p + j) = temp;
+				int* pi = *(p + i);
+				int* pj = *(p + j);
+				swap(&pi, &pj);
 			}
 		}
 	}
 }
+
 int main()
 {
 	int arr[5] = { 5, 7, 2, 3, 4 };
 	int* p_arr = arr[5];
-	int temp;
 	for (int i = 0; i < 5; i++)
 	{
 		printf("%d ", arr[i]);
@@ -25,9 +32,10 @@ int main()
 	for (int i = 0; i < 5 - 1; i++) {
 		for (int j = i + 1; j < 5; j++) {
 			if (arr[i] > arr[j]) {
-				temp = arr[i];
+				int test = arr[i];
 				arr[i] = arr[j];
-				arr[j] = temp;
+				arr[j] = test;
+
 			}
 		}
 	}
